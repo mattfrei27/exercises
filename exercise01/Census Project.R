1baef84b3eda41c8620ad05a2278474878966a82
Matt Frei <mattfrei27@gmail.com>
2015-02-06 18:42:39 -0500

    Finalized analysis

--- exercise01/Census Project.R
+++ exercise01/Census Project.R
@@ -14,7 +14,10 @@ library(car) #vif
 library(vcd) #Tests of association
 library(pROC) #ROC curve
 library(ggplot2)
-library(tree) #CART
+library(tree) #CART 1
+library(rpart) #More sophisticated decision tree algorithm
+library(rattle) #Decision tree plotting
+library(rpart.plot) #Decision tree plotting
 
 ##################################### ACCESS CENSUS DATA IN SQLITE DATABASE. FLATTEN AND EXPORT TO CSV #####################################
 
@@ -108,12 +111,16 @@ data$ageCat1 <- cut(data$age,breaks=c(-Inf,29,49,64,Inf),labels=c('17-29','30-49
 
 
 data$marital2 <- revalue(data$marital,c("Married-AF-spouse"="Married","Married-civ-spouse"="Married"))
-data$native <- data$country == "United-States"
+data$native <- data$country != "United-States"
 data$capGainDummy <- data$capital_gain > 0
 data$capLossDummy <- data$capital_loss > 0
 data$capGainLoss <- data$capital_gain - data$capital_loss
-
+data$hasInvest <- factor(data$capGainLoss != 0)
 # data$agelogage <- data$age*log(data$age) #Box Tidewell Transformation
+
+data$race <- relevel(data$race,ref=5)
+data$gender <- relevel(data$gender,ref=2)
+data$ageCat1 <- relevel(data$ageCat1,ref=2)
 
 
 
@@ -136,20 +143,19 @@ test <- notTrain[-validateIndex,] #Rows that aren't in training or validation ar
 rm(notTrain)
 
 #Confirm that the target variable is relatively balanced across the partitions
-CrossTable(train$over_50k)
-CrossTable(validate$over_50k)
-CrossTable(test$over_50k)
+# CrossTable(train$over_50k)
+# CrossTable(validate$over_50k)
+# CrossTable(test$over_50k)
 
 
 #####Logistic regression
 # options("scipen"=100, "digits"=4) #Makes printing scientific notation less likely
 
-model3 <- glm(over_50k~ageCat1+workclass2+educ2+marital2+occup+race+gender+capGainLoss+hours_week+native, data=train, family="binomial")
+model3 <- glm(over_50k~ageCat1+workclass2+educ2+marital2+occup+race+gender+poly(capGainLoss,2)+hours_week+native, data=train, family="binomial")
 summary(model3)
-OR <- exp(coef(model3)[-1])
-OR
-OR.CI <- exp(cbind(OR = coef(model3), confint(model3)))[-1,]
-OR.CI
+OR <- exp(coef(model3))
+sort(OR)
+
 
 #ROC Curve for validation data
 fitpreds = predict(model3,newdata=validate,type="response")
@@ -158,28 +164,42 @@ print(ROC_valid)
 plot(ROC_valid)
 
 #Since neither type of error is more "costly" here, I'll use the Youden index to choose a cutoff
-coords(ROC_valid,x='best',best.method="youden") #.2292
-ggplot(validate, aes(x=fitpreds, fill=over_50k)) + geom_density(alpha=.3) + geom_vline(xintercept=.2292, linetype="dotted")
-decision <- ifelse(fitpreds >=.2292,1,0)
-mean(decision != validate$over_50k) #19.8% missclassification
+fitpreds_test = predict(model3,newdata=test,type="response")
+coords(ROC_valid,x='best',best.method="youden") #.2265
+ggplot(test, aes(x=fitpreds_test, fill=over_50k)) + geom_density(alpha=.3) + geom_vline(xintercept=.2265, linetype="dotted")
+decision <- ifelse(fitpreds_test >=.2265,1,0)
+mean(decision != test$over_50k) #~20% missclassification in test data
 
 # model1_sw <- step(model3)
 # summary(model1_sw) #All variables kept
 
 
-
 #####Decision tree approach
-maximalTree <- tree(over_50k~.,train[,!names(train) %in% c("id","country")])
-plot(maximalTree)
-text(maximalTree, pretty=0)
+excludedVars <- c("id","country","educ","education_num","relation","relation2")
 
-set.seed(144627)
-cv_tree <- cv.tree(maximalTree,FUN=prune.misclass)
-plot(cv_tree$size,cv_tree$dev,type="b") #5 leaves is adequate
+tree.2 <- rpart(over_50k~.,data=train[,!names(train) %in% excludedVars])								
+fancyRpartPlot(tree.2)
+tree_pred <- predict(tree.2,test,type="class")
+mean(tree_pred != test$over_50k) #15-16% missclassification (validation vs. testing)
 
-prunnedTree <- prune.misclass(maximalTree,best=5)
-plot(prunnedTree)
-text(prunnedTree,pretty=0)
+#Run tree on all the data. We get the same model
+tree.2 <- rpart(over_50k~.,data=data[,!names(data) %in% excludedVars])  						
+fancyRpartPlot(tree.2)
 
-tree_pred = predict(prunnedTree,validate,type="class")
-mean(tree_pred != validate$over_50k) #15.1% missclassification
+
+
+##OLD Decision Tree Code
+# maximalTree <- tree(over_50k~.,train[,!names(train) %in% excludedVars])
+# # plot(maximalTree)
+# # text(maximalTree, pretty=0)
+# 
+# set.seed(144627)
+# cv_tree <- cv.tree(maximalTree,FUN=prune.misclass)
+# plot(cv_tree$size,cv_tree$dev,type="b") #5 leaves is adequate
+# 
+# prunnedTree <- prune.misclass(maximalTree,best=5)
+# plot(prunnedTree)
+# text(prunnedTree,pretty=0)
+# 
+# tree_pred <- predict(prunnedTree,validate,type="class")
+# mean(tree_pred != validate$over_50k) #15.1% missclassification
