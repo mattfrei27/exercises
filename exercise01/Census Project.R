6ca5eaa9946809246bdae76ef2492a256190be6a
Matt Frei <mattfrei27@gmail.com>
2015-02-05 22:36:50 -0500

    Models Built

--- exercise01/Census Project.R
+++ exercise01/Census Project.R
@@ -1,12 +1,27 @@
-dbPath <- 'C:/Users/matt/Dropbox/Documents/Resume and Applications/applications/rti/assessment/exercise01/exercise01.sqlite'
+#Code written by Matt Frei in Feb 2015 for RTI Application Process
 
 #install.packages("RSQLite")
 #install.packages("gmodels")
 #install.packages("plyr")
+#install.packages("vcd")
+#install.packages('pROC')
+#install.packages('tree')
 
 library(DBI) #Connection to a SQLLite database
 library(gmodels) #Nice crosstabs with %s
-library(plyr) #Recoding variables
+library(plyr) #Recoding variables 
+library(car) #vif
+library(vcd) #Tests of association
+library(pROC) #ROC curve
+library(ggplot2)
+library(tree) #CART
+
+##################################### ACCESS CENSUS DATA IN SQLITE DATABASE. FLATTEN AND EXPORT TO CSV #####################################
+
+
+dbPath <- 'C:/Users/matt/Dropbox/Documents/Resume and Applications/applications/rti/assessment/exercise01/exercise01.sqlite'
+csv <- "C:/Users/matt/Dropbox/Documents/Resume and Applications/Applications/RTI/Assessment/exercise01/census.csv"
+
 
 #Connect to database
 con <- dbConnect(RSQLite::SQLite(), dbPath)
@@ -45,29 +60,70 @@ dbClearResult(res)
 dbDisconnect(con)
 
 #Write the dataframe to csv
-csv <- "C:/Users/matt/Dropbox/Documents/Resume and Applications/Applications/RTI/Assessment/exercise01/census.csv"
 write.csv(results,file=csv,row.names=F)
+
+
+
+
+
+
+##################################### READ IN CSV and PREP DATA #####################################
+
 
 #Load data from csv. ? refers to a missing value in the original database
 data <- read.csv(csv,na.strings=c("?"))
 
-summary(data)
-apply(data[,c(3:10,14)],2,table) #Frequency tables for categorical variables
+# 
+# table(complete.cases(data)) #7% of cases have at least one missing value
+# M <- sapply(data, function(x) sum(is.na(x))) 
+# M[M>0] #occup, workclass, and country have missing values
+
+# summary(data)
+# apply(data[,c(3:10,14)],2,table) #Frequency tables for categorical variables
 
 #Variable recoding and preparation
+work_char <- as.character(data$workclass) #I'll treat missing values as a level to avoid dropping observations
+work_char[is.na(work_char)] <- 'Missing'
+data$workclass <- factor(work_char)
+
+occup_char <- as.character(data$occup)
+occup_char[is.na(occup_char)] <- 'Missing'
+data$occup <- factor(occup_char)
+
+country_char <- as.character(data$country)
+country_char[is.na(country_char)] <- 'Missing'
+data$country <- factor(country_char)
+
 data$over_50k <- factor(data$over_50k)
+# CrossTable(data$over_50k) # 24% high income. 76% low.
 data$workclass2 <- revalue(data$workclass,c("Never-worked"="None","Without-pay"="None"))
 #table(data$educ,data$education_num)
 data$educ2 <- cut(data$education_num,breaks=c(-Inf,8,9,12,Inf),labels=c('LT HS','HS Grad','Some College','College Grad+'))
 #table(data$educ2,data$education_num)
+relation_char <- as.character(data$relation)
+relation_char_recoded <- ifelse(relation_char == "Husband" | relation_char == "Wife","Wife/Husband","Other")
+#relation_char_recoded <- sapply(relation_char,function(x) if (x == "Husband" | x == "Wife") x else "Other") #This line is equivelant to the previous one
+data$relation2 <- factor(relation_char_recoded)
+data$ageCat1 <- cut(data$age,breaks=c(-Inf,29,49,64,Inf),labels=c('17-29','30-49','50-64','65+'))
+
+
 data$marital2 <- revalue(data$marital,c("Married-AF-spouse"="Married","Married-civ-spouse"="Married"))
 data$native <- data$country == "United-States"
 data$capGainDummy <- data$capital_gain > 0
 data$capLossDummy <- data$capital_loss > 0
 data$capGainLoss <- data$capital_gain - data$capital_loss
 
+# data$agelogage <- data$age*log(data$age) #Box Tidewell Transformation
 
-#Split the data into training, validation, and test
+
+
+
+
+
+##################################### PARTITION DATA AND MODEL #####################################
+
+
+#Split the data into training (70%), validation (20%), and test (10%)
 set.seed(452851)
 index <- 1:nrow(data)
 trainIndex <- sample(index, round(.7*nrow(data))) #Randomly select subset of rows for inclusion in training set
@@ -84,9 +140,46 @@ CrossTable(train$over_50k)
 CrossTable(validate$over_50k)
 CrossTable(test$over_50k)
 
-#Logistic regression
-model1 <- glm(over_50k~poly(age,2)+workclass2+educ2+marital2+occup+race+gender+capGainLoss+hours_week+native, data=train, family="binomial")
-summary(model1)
-exp(coef(model1)) #Odds ratios
 
-#Note to self: Finalize model, check assumptions, continue
+#####Logistic regression
+# options("scipen"=100, "digits"=4) #Makes printing scientific notation less likely
+
+model3 <- glm(over_50k~ageCat1+workclass2+educ2+marital2+occup+race+gender+capGainLoss+hours_week+native, data=train, family="binomial")
+summary(model3)
+OR <- exp(coef(model3)[-1])
+OR
+OR.CI <- exp(cbind(OR = coef(model3), confint(model3)))[-1,]
+OR.CI
+
+#ROC Curve for validation data
+fitpreds = predict(model3,newdata=validate,type="response")
+ROC_valid <- roc(validate$over_50k, fitpreds)
+print(ROC_valid)
+plot(ROC_valid)
+
+#Since neither type of error is more "costly" here, I'll use the Youden index to choose a cutoff
+coords(ROC_valid,x='best',best.method="youden") #.2292
+ggplot(validate, aes(x=fitpreds, fill=over_50k)) + geom_density(alpha=.3) + geom_vline(xintercept=.2292, linetype="dotted")
+decision <- ifelse(fitpreds >=.2292,1,0)
+mean(decision != validate$over_50k) #19.8% missclassification
+
+# model1_sw <- step(model3)
+# summary(model1_sw) #All variables kept
+
+
+
+#####Decision tree approach
+maximalTree <- tree(over_50k~.,train[,!names(train) %in% c("id","country")])
+plot(maximalTree)
+text(maximalTree, pretty=0)
+
+set.seed(144627)
+cv_tree <- cv.tree(maximalTree,FUN=prune.misclass)
+plot(cv_tree$size,cv_tree$dev,type="b") #5 leaves is adequate
+
+prunnedTree <- prune.misclass(maximalTree,best=5)
+plot(prunnedTree)
+text(prunnedTree,pretty=0)
+
+tree_pred = predict(prunnedTree,validate,type="class")
+mean(tree_pred != validate$over_50k) #15.1% missclassification
